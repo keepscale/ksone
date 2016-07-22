135003a183fe77ecb47a3fadf40618ad5fc9e627
lgangloff <l.gangloff@gmail.com>
2016-07-22 17:06:04 +0200

    suite

--- src/main/webapp/scripts/components/alert/alert.directive.js
+++ src/main/webapp/scripts/components/alert/alert.directive.js
@@ -5,7 +5,7 @@ angular.module('crossfitApp')
 		return {
             restrict: 'E',
             template: '<div class="alerts" ng-cloak="">' +
-			                '<alert ng-cloak="" ng-repeat="alert in alerts" type="{{alert.type}}" close="alert.close()"><pre>{{ alert.msg }}</pre></alert>' +
+			                '<div ng-cloak="" ng-repeat="alert in alerts" type="{{alert.type}}" close="alert.close()"><pre>{{ alert.msg }}</pre></div>' +
 			            '</div>',
 			controller: ['$scope', 
 	            function($scope) {
@@ -21,7 +21,7 @@ angular.module('crossfitApp')
 		return {
             restrict: 'E',
             template: '<div class="alerts" ng-cloak="">' +
-			                '<alert ng-cloak="" ng-repeat="alert in alerts" type="{{alert.type}}" close="alert.close()"><pre>{{ alert.msg }}</pre></alert>' +
+			                '<div ng-cloak="" ng-repeat="alert in alerts" type="{{alert.type}}" close="alert.close()"><pre>{{ alert.msg }}</pre></div>' +
 			            '</div>',
 			controller: ['$scope', 
 	            function($scope) {
