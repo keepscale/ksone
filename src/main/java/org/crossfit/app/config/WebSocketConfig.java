31785c9e0543b2df6641c2dd7111c8c4aaa1f554
lgangloff <lgangloff>
2018-05-12 01:13:28 +0200

    setting sockjs url

--- src/main/java/org/crossfit/app/config/WebSocketConfig.java
+++ src/main/java/org/crossfit/app/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws");
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").withSockJS().setClientLibraryUrl("/bower_components/sockjs-client/dist/sockjs.min.js");
     }
 }
