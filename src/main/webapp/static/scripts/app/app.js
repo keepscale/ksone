b6d17bf3552e13606fea26aa4901133eec06083f
lgangloff <lgangloff>
2018-05-14 13:55:41 +0200

    upgrade js version

--- src/main/webapp/static/scripts/app/app.js
+++ src/main/webapp/static/scripts/app/app.js
@@ -59,7 +59,7 @@ angular.module('crossfitApp', ['LocalStorageModule', 'tmh.dynamicLocale', 'pasca
     })
     .config(function ($compileProvider, $stateProvider, $urlRouterProvider, $httpProvider, $locationProvider, $translateProvider, tmhDynamicLocaleProvider, httpRequestInterceptorCacheBusterProvider) {
 
-    	$stateProvider.VERSION = "5";
+    	$stateProvider.VERSION = "6";
         
         //enable CSRF
         $httpProvider.defaults.xsrfCookieName = 'CSRF-TOKEN';
