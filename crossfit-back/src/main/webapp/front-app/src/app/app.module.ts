4a68e9ea54ec19d6c6f68d3c2379d7e23d2f89ed
lgangloff <lgangloff>
2018-05-25 17:20:24 +0200

    [i18n] add i18n feature

--- crossfit-back/src/main/webapp/front-app/src/app/app.module.ts
+++ crossfit-back/src/main/webapp/front-app/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
@@ -22,7 +26,7 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatIconModule,
     MatListModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'fr' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
