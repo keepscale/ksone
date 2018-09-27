5a2e113603439f92882c456b2791ca7355b1fd40
lgangloff <lgangloff>
2018-09-27 12:04:55 +0200

    remove sensible rest endpoint

--- crossfit-back/src/main/java/org/crossfit/app/web/rest/api/CrossFitBoxResource.java
+++ crossfit-back/src/main/java/org/crossfit/app/web/rest/api/CrossFitBoxResource.java
@@ -41,23 +41,6 @@ public class CrossFitBoxResource {
 
     @Inject
     private CrossFitBoxSerivce boxService;
-    
-    /**
-     * POST  /boxs -> Create a new crossFitBox.
-     */
-    @RequestMapping(value = "/boxs",
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CrossFitBox> create(@Valid @RequestBody CrossFitBox crossFitBox) throws URISyntaxException {
-        log.debug("REST request to save CrossFitBox : {}", crossFitBox);
-        if (crossFitBox.getId() != null) {
-            return ResponseEntity.badRequest().header("Failure", "A new crossFitBox cannot already have an ID").body(null);
-        }
-        CrossFitBox result = crossFitBoxRepository.save(crossFitBox);
-        return ResponseEntity.created(new URI("/api/boxs/" + result.getId()))
-                .headers(HeaderUtil.createEntityCreationAlert("crossFitBox", result.getId().toString()))
-                .body(result);
-    }
 
     /**
      * PUT  /boxs -> Updates an existing crossFitBox.
@@ -68,57 +51,16 @@ public class CrossFitBoxResource {
     public ResponseEntity<CrossFitBox> update(@Valid @RequestBody CrossFitBox crossFitBox) throws URISyntaxException {
         log.debug("REST request to update CrossFitBox : {}", crossFitBox);
         if (crossFitBox.getId() == null) {
-            return create(crossFitBox);
+            return ResponseEntity.badRequest().build();
         }
         CrossFitBox result = crossFitBoxRepository.save(crossFitBox);
         return ResponseEntity.ok()
                 .headers(HeaderUtil.createEntityUpdateAlert("crossFitBox", crossFitBox.getId().toString()))
                 .body(result);
     }
-
-    /**
-     * GET  /boxs -> get all the crossFitBoxs.
-     */
-    @RequestMapping(value = "/boxs",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<CrossFitBox>> getAll(@RequestParam(value = "page" , required = false) Integer offset,
-                                  @RequestParam(value = "per_page", required = false) Integer limit)
-        throws URISyntaxException {
-        Page<CrossFitBox> page = crossFitBoxRepository.findAll(PaginationUtil.generatePageRequest(offset, limit));
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/boxs", offset, limit);
-        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
-    }
-
-    /**
-     * GET  /boxs/:id -> get the "id" crossFitBox.
-     */
-    @RequestMapping(value = "/boxs/{id}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CrossFitBox> get(@PathVariable Long id) {
-        log.debug("REST request to get CrossFitBox : {}", id);
-        return Optional.ofNullable(crossFitBoxRepository.findById(id).get())
-            .map(crossFitBox -> new ResponseEntity<>(
-                crossFitBox,
-                HttpStatus.OK))
-            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-
-    /**
-     * DELETE  /boxs/:id -> delete the "id" crossFitBox.
-     */
-    @RequestMapping(value = "/boxs/{id}",
-            method = RequestMethod.DELETE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        log.debug("REST request to delete CrossFitBox : {}", id);
-        crossFitBoxRepository.deleteById(id);
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("crossFitBox", id.toString())).build();
-    }
     
     /**
-     * GET  /boxs/:id -> get the "id" crossFitBox.
+     * GET  /boxs/current -> get the "id" crossFitBox.
      */
     @RequestMapping(value = "/boxs/current",
             method = RequestMethod.GET,
