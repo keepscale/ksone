3b12c83ada9dd3626889e29ce097499607b170fe
lgangloff <lgangloff>
2018-10-09 17:14:47 +0200

    events cell

--- crossfit-back/src/main/webapp/front-app/src/app/events/events.module.ts
+++ crossfit-back/src/main/webapp/front-app/src/app/events/events.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { CalendarComponent } from './calendar/calendar.component';
 import { EventsTableComponent } from './events-table/events-table.component';
+import { EventsCellComponent } from './events-cell/events-cell.component';
 
 @NgModule({
   imports: [
@@ -12,6 +13,6 @@ import { EventsTableComponent } from './events-table/events-table.component';
   exports: [
     CalendarComponent
   ],
-  declarations: [CalendarComponent, EventsTableComponent]
+  declarations: [CalendarComponent, EventsTableComponent, EventsCellComponent]
 })
 export class EventsModule { }
