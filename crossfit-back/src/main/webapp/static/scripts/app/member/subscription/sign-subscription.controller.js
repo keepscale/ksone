8394ba1465ea9b6f8bc74bfd12b42aca1bc26448
lgangloff <lgangloff>
2019-03-07 14:01:06 +0100

    printing sepa information

--- crossfit-back/src/main/webapp/static/scripts/app/member/subscription/sign-subscription.controller.js
+++ crossfit-back/src/main/webapp/static/scripts/app/member/subscription/sign-subscription.controller.js
@@ -2,12 +2,13 @@
 
 angular.module('crossfitApp')
     .controller('SignSubscriptionController', 
-    		['$scope', '$stateParams', '$state', 'Member', 'Subscription',
-	function($scope, $stateParams, $state, Member, Subscription) {
+    		['$scope', '$stateParams', '$state', '$rootScope', 'Member', 'Subscription',
+	function($scope, $stateParams, $state, $rootScope, Member, Subscription) {
 
         $scope.init = function(){
         	$scope.member = Member.get({id : $stateParams.memberId});
         	$scope.sub = Subscription.get({id : $stateParams.subscriptionId});
+        	$scope.logoUrl = $rootScope.box.billLogoUrl;
         }
         
         $scope.readAndApproved = function(){
