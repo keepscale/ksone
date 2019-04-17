7292ecf6199b7802736706c9c0ae7291cfca3407
lgangloff <lgangloff>
2019-04-17 13:12:15 +0200

    suite mandate

--- crossfit-back/src/main/webapp/static/scripts/app/signature/sign-mandate.controller.js
+++ crossfit-back/src/main/webapp/static/scripts/app/signature/sign-mandate.controller.js
@@ -5,6 +5,22 @@ angular.module('crossfitApp')
     		['$scope', '$stateParams', '$state', '$rootScope', 'Member', 'Signature',
 	function($scope, $stateParams, $state, $rootScope, Member, Signature) {
 
+    	$scope.box = {
+    		sepa : {
+    			logoURL: "/assets/images/sepa-logo.png",
+    			headerTitle: "Mandat de prélèvement SEPA",
+    			legalText: "En signant ce formulaire de mandat, vous autorisez la société KOOPER inc - CROSSFIT NANCY Affiliate\
+							à envoyer des instructions à votre banque pour débiter votre compte,\
+							et votre banque à débiter votre compte conformément aux instructions de KOOPER inc - CROSSFIT NANCY Affiliate.\
+							Vous bénéficiez du droit d’être remboursé par votre banque selon les conditions décrites dans la convention que vous avez passée avec elle.\
+							<br/>\
+							Une demande de remboursement doit être présentée : dans les 8 semaines suivant la date de débit de votre compte pour un prélèvement autorisé,\
+							sans tarder et au plus tard dans les 13 mois en cas de prélèvement non authorisé,\
+							et vos droits concernant le présent mandat sont expliqués dans un document que vous pouvez obtenir auprès de votre banque.",
+        		beneficiaire : "<b>Kooper inc</b> <br/>24/26 bd du 26ème Régiment d’Infanterie <br/>54000 Nancy"
+    		}		
+    	}
+    			
         $scope.init = function(){
         	
         	$scope.logoUrl = $rootScope.box.billLogoUrl;
@@ -21,10 +37,12 @@ angular.module('crossfitApp')
             });
         	
         }
-
+    	
+    	
         $scope.sign = function(){
-            Signature.signmandate($scope.mandateSigned);
-            $scope.init();
+            Signature.signmandate($scope.mandateSigned, function(){;
+            	$scope.init();
+            })
         }
         
         $scope.init();
