1b042644d0b0e77f25d63bca2f80e4c8ef95ed73
Loic Gangloff <loic.gangloff@gmail.com>
2020-06-25 13:46:23 +0200

    wod picker

--- crossfit-back/src/main/webapp/front-app/src/app/wod/wod-dialog-picker/wod-dialog-picker.component.ts
+++ crossfit-back/src/main/webapp/front-app/src/app/wod/wod-dialog-picker/wod-dialog-picker.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { PaginateList } from 'src/app/common/paginate-list.model';
-import { Wod } from '../domain/wod.model';
-import { WodSearchRequest } from '../wod.service';
+import { RunnerService } from 'src/app/common/runner.service';
+import { Wod, WodPublication } from '../domain/wod.model';
+import { WodSearchRequest, WodService } from '../wod.service';
+import { SelectionModel } from '@angular/cdk/collections';
 
 
 export interface WokDialogPickerComponentData {
@@ -16,14 +18,28 @@ export interface WokDialogPickerComponentData {
 })
 export class WoDDialogPickerComponent implements OnInit {
 
-  list:PaginateList<Wod>;
-  search = new WodSearchRequest();
-  
+  selection = new SelectionModel<Wod>(false, []);
+
   constructor(
     public dialogRef: MatDialogRef<WoDDialogPickerComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: WokDialogPickerComponentData) {}
+    @Inject(MAT_DIALOG_DATA) public data: WokDialogPickerComponentData,
+    protected runner: RunnerService<Wod>,
+    protected service: WodService) {}
 
   ngOnInit() {
   }
 
+  close() {
+    this.dialogRef.close();
+  }
+
+  addPublication(){
+    let wod = this.selection.selected[0];
+    wod.publications.push(new WodPublication(this.data.date));
+    
+    this.runner.run(
+      this.service.save(wod),
+      res=>this.close()
+    )
+  }
 }
