8937977f739ece0695c9b2803951a38a1a135ab2
Loic Gangloff <loic.gangloff@gmail.com>
2020-06-26 13:27:40 +0200

    decorative for angular 10

--- crossfit-back/src/main/webapp/front-app/src/app/common/abstract-detail.component.ts
+++ crossfit-back/src/main/webapp/front-app/src/app/common/abstract-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive } from '@angular/core';
 import { ToolBarService } from 'src/app/toolbar/toolbar.service';
 import { Principal } from 'src/app/shared/auth/principal.service';
 import { User } from '../shared/domain/user.model';
 import { AbstractComponent } from './abstract.component';
 
+@Directive()
 export class AbstractDetailComponent extends AbstractComponent implements OnInit {
 
   ngOnInit() {
